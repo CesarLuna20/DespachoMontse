ed00f62bb1a301e8b503c19b5dd00b550e85529d
CesarLuna20 <isccluna017@accitesz.com>
2026-08-08 09:43:43 -0600

    Añade descripción concisa de los cambios

--- .history/artifacts/api-server/src/app_20260808094155.ts
+++ .history/artifacts/api-server/src/app_20260808094155.ts
@@ -1,6 +1,6 @@
 import express, { type Express } from "express";
 import cors from "cors";
-import pinoHttp = require("pino-http");
+import pinoHttp from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
 
@@ -10,19 +10,14 @@ app.use(
   pinoHttp({
     logger,
     serializers: {
-      req(req) {
-        return {
-          id: req.id,
-          method: req.method,
-          url: req.url?.split("?")[0],
-        };
-      },
-
-      res(res) {
-        return {
-          statusCode: res.statusCode,
-        };
-      },
+      req: (req) => ({
+        id: req.id,
+        method: req.method,
+        url: req.url?.split("?")[0],
+      }),
+      res: (res) => ({
+        statusCode: res.statusCode,
+      }),
     },
   }),
 );
